1ee3485f0b6be1877126853cc3c445520e62640d
ActualGEO <gbadamosioluwaobi10@gmai.com>
2025-12-19 07:28:32 +0100

    modified my models.py

--- PollApp/poll/models.py
+++ PollApp/poll/models.py
@@ -6,22 +6,26 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length=300)
+    poll_question = models.CharField(max_length=300)
     publication_date = models.DateTimeField()
+    is_active = models.BooleanField(default=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
 
     def __str__(self):
-        return self.question_text
+        return self.poll_question
     
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=100)
+    poll_choice = models.CharField(max_length=100)
 
 
 class Vote(models.Model):
+    timestamp = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     choice = models.ForeignKey(Choice, on_delete=models.CASCADE)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
 
     def __str__(self):
         return f"{self.user} voted {self.choice}"
