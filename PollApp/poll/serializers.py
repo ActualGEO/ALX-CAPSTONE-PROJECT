2b3c00f43192496721cd490c9e04c344ce09354b
ActualGEO <gbadamosioluwaobi10@gmai.com>
2025-12-21 20:03:43 +0100

    in my serializer file i defined relationship between the models

---
+++ PollApp/poll/serializers.py
@@ -0,0 +1,30 @@
+from .models import Question, User, Choice, Vote
+from rest_framework import serializers
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class QuestionSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    class Meta:
+        model = Question
+        fields = '__all__'
+
+
+class ChoiceSerializer(serializers.ModelSerializer):
+    question = QuestionSerializer(read_only=True)
+    class Meta:
+        model = Choice
+        fields = '__all__'
+
+
+class VoteSerializers(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    question = QuestionSerializer(read_only=True)
+    choice = ChoiceSerializer(read_only=True)
+    class Meta:
+        model = Vote
+        fields = '__all__'
